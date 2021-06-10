92847e62d184d16f3a22865628840fd4fb24bc87
ardimasyrofi <72204907+ardimasyrofi@users.noreply.github.com>
2021-06-10 14:02:00 +0700

    60 %

--- kasir.py
+++ kasir.py
@@ -1,8 +1,10 @@
 import wx
 import wx.xrc
 import wx.grid
-import conn
 import View
+
+conn = mysql.connector.connect(host="localhost",port=3306,user="root",password="",database="pbo2_toko")
+curs = conn.cursor()
 
 class Kasir(View.KasirFrame):
     def __init__(self, parent):
@@ -10,17 +12,42 @@ class Kasir(View.KasirFrame):
         
     def showTransaksi(self,event):
         self.Hide()
-        frame=DataTransaksiFrame(None)
+        frame=DataTransaksi(None)
         frame.Show()
 
     def createTransaksi(self,event):
         self.Hide()
-        frame=TambahTransaksiFrame(None)
+        frame=TambahTransaksi(None)
         frame.Show()
 
     def showLaporan(self,event):
         self.Hide()
         #dialog
+        
     def exit(self,event):
         event.Skip()
+
+class DataTransaksi(View.DataTokoFrame):
+    def back( self,event):
+        self.subframe = Pemilik(None)
+        self.subframe.Show()
+        self.Destroy()
+
+class TambahTransaksi(View.TambahTokoFrame):
+	def __init__(self, parent):
+		super().__init__(parent)
+		
+	def back( self,event):
+		self.subframe = Pemilik(None)
+		self.subframe.Show()
+		self.Destroy()
+
+	def createToko( self, event ):
+		alamat = self.toko_input.GetValue()
+		query = f'INSERT INTO toko VALUES("", "{alamat}")'
+		curs.execute(query)
+		conn.commit()
+		wx.MessageBox(f'Berhasil Menambahkan!')
+		
+
         
