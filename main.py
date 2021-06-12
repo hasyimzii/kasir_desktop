1478615755d2a46c56c57ced382f021daa675263
ard'sss <72204907+ardimasyrofi@users.noreply.github.com>
2021-06-12 13:06:21 +0700

    fix bug

--- main.py
+++ main.py
@@ -17,28 +17,29 @@ class Login(View.LoginFrame):
         query = f'SELECT * FROM user WHERE username = "{username}" AND password = "{password}"'
         curs.execute(query)
         data = curs.fetchall()
-        if data[0][3] == "pemilik":
-            from Pemilik import Pemilik
-            self.Destroy()
-            app = wx.App()
-            frame = Pemilik(None)
-            frame.Show()
-            app.MainLoop()
-        elif data[0][3] == "manager":
-            from Manager import Manager
-            self.Destroy()
-            app = wx.App()
-            frame = Manager(None)
-            frame.Show()
-            app.MainLoop()
-        elif data[0][3] == "kasir":
-            from Kasir import Kasir
-            self.Destroy()
-            app = wx.App()
-            frame = Kasir(parent=None, idUser=data[0][0])
-            frame.Show()
-            app.MainLoop()
-        else:
+        try:
+            if data[0][3] == "pemilik":
+                from pemilik import Pemilik
+                self.Destroy()
+                app = wx.App()
+                frame = Pemilik(None)
+                frame.Show()
+                app.MainLoop()
+            elif data[0][3] == "manager":
+                from Manager import Manager
+                self.Destroy()
+                app = wx.App()
+                frame = Manager(None)
+                frame.Show()
+                app.MainLoop()
+            elif data[0][3] == "kasir":
+                from kasir import Kasir
+                self.Destroy()
+                app = wx.App()
+                frame = Kasir(parent=None, idUser=data[0][0])
+                frame.Show()
+                app.MainLoop()
+        except:
             wx.MessageBox(f'Username dan Password Salah')
 
 app = wx.App()
